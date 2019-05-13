d0c072c2e188502dce23019577cf18acd922492f
Unknown <gaby.tg96@gmail.com>
2019-05-13 07:44:08 +0300

    local and remote functionalities added to the code
    TODO: update the ReadMes.

--- Simulations/run_simulated_navigation.sh
+++ Simulations/run_simulated_navigation.sh
@@ -4,7 +4,7 @@ my_pid=$$
 echo "My process ID is $my_pid"
 
 echo "Launching Gazebo..."
-roslaunch pioneer_gazebo gazebo_world.launch world:="test_4" &
+roslaunch pioneer_gazebo gazebo_world.launch world:="maze_one" &
 pid="$pid $!"
 sleep 5s
 
@@ -14,7 +14,7 @@ pid="$pid $!"
 sleep 2s
 
 echo "Launching Pioneers in Gazebo stack..."
-for i in `seq 1 2`;
+for i in `seq 1 1`;
 do
   roslaunch pioneer_description pioneer_description.launch robot_name:="pioneer$i" robot_pose:="-x $(rosparam get /pioneer$i/x) -y $(rosparam get /pioneer$i/y) -Y $(rosparam get /pioneer$i/a)" environment:="gazebo" use_real_kinect:=false kinect_to_laserscan:=true &
   pid="$pid $!"
@@ -22,22 +22,22 @@ do
 done
 
 echo "Launching map server..."
-roslaunch pioneer_nav2d map_server.launch map_name:="test_4" &
+roslaunch pioneer_nav2d map_server.launch map_name:="maze_one" &
 pid="$pid $!"
 sleep 2s
 
 echo "Launching AMCL localisation stack..."
-for i in `seq 1 2`;
+for i in `seq 1 1`;
 do
-  roslaunch pioneer_nav2d localisation_launcher.launch robot_name:="pioneer$i" robot_pose_x:="$(rosparam get /pioneer$i/x)" robot_pose_y:="$(rosparam get /pioneer$i/y)" robot_pose_yaw:="$(rosparam get /pioneer$i/a)" localization_type:="fake_localization" &
+  roslaunch pioneer_nav2d localisation_launcher.launch robot_name:="pioneer$i" robot_pose_x:="$(rosparam get /pioneer$i/x)" robot_pose_y:="$(rosparam get /pioneer$i/y)" robot_pose_yaw:="$(rosparam get /pioneer$i/a)" localization_type:="amcl" &
   pid="$pid $!"
   sleep 2s
 done
 
 echo "Launching move_base stack..."
-for i in `seq 1 2`;
+for i in `seq 1 1`;
 do
-  roslaunch pioneer_nav2d move_base.launch robot_name:="pioneer$i" move_base_type:="move_base" base_global_planner:="NavfnROS" base_local_planner:="TrajectoryPlannerROS" mcp_use:=false &
+  roslaunch pioneer_nav2d move_base.launch robot_name:="pioneer$i" move_base_type:="move_base" base_global_planner:="NavfnROS" base_local_planner:="TebLocalPlannerROS" mcp_use:=false &
   pid="$pid $!"
   sleep 2s
 done
